413242f829a8db16656c47f0731550e422904f91
BamUnd <chelseablues36@gmail.com>
2018-09-24 08:10:59 +0900

    getelementsbynameの練習

--- main.js
+++ main.js
@@ -6,10 +6,15 @@ document.addEventListener('DOMContentLoaded',function(){
         let name = document.getElementById('name');
         let output = document.getElementById("nameoutput");
         let output2 = document.getElementById("nameoutput2");
+        let output3 = document.getElementById("nameoutput3");
+        let list = document.getElementsByName('name');
 
         //innnerText,innerHTMLの挙動違い確認。
         output.innerText= name.value;
         output2.innerHTML = name.value;
+
+        //NodeListOf<HTMLElement>の取り扱い練習。
+        output3.innerText = list.item(0).value;
     },false)
     document.getElementById('container').addEventListener("click",function(){
 
